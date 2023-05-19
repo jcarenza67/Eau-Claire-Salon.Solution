a8dbde66b784b3917489de120598d3a5b22816de
Joseph Wilfong <josephwilfong91@gmail.com>
2023-05-19 12:06:27 -0700

    add some form validation

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -1,10 +1,21 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+
 namespace HairSalon.Models
 {
   public class Client
   {
     public int ClientId { get; set; }
+
+    [Required]
     public string Name { get; set; }
+
+    [Required]
     public string Phone { get; set; }
+
+    [Required]
+    [EmailAddress]
     public string Email { get; set; }
     public string Description { get; set; }
     public int StylistId { get; set; }
